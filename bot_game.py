a1b25817e22e5c94bba6fbc3d73500d9ce683dfe
mobiaks2304 <lotockiy.matviy@gmail.com>
2025-05-27 19:11:56 +0300

    guess_number

---
+++ bot_game.py
@@ -0,0 +1,23 @@
+import telebot,os
+from dotenv import load_dotenv
+from random import randint
+load_dotenv()
+
+BOT_API = os.getenv('bot_api')
+bot = telebot.TeleBot(BOT_API)
+number = randint(1,10)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Здоров, попробуй вгадати число від 1 до 10')
+
+@bot.message_handler(func=lambda message: message.text.isdigit())
+def guess_number(message):
+    if int(message.text) == number:
+        bot.send_message(message.chat.id, 'Ти виграв!')
+    else:
+        bot.send_message(message.chat.id, 'Ти програв')
+
+
+bot.polling(none_stop=True)
